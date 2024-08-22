e6a79688852a3f2953343b5e08e1dc639adb8587
EArkharov <egorarkharov@gmail.com>
2024-08-22 20:48:04 +0400

    Base settings

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: ["plugin:vue/vue3-recommended", "eslint:recommended"],
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: "module",
+  },
+  rules: {
+    "vue/no-multiple-template-root": "off",
+  },
+};
--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,20 @@
-const { defineConfig } = require("@vue/cli-service");
-module.exports = defineConfig({
+const path = require('path');
+
+module.exports = {
   transpileDependencies: true,
   lintOnSave: false,
-});
+  css: {
+    loaderOptions: {
+      sass: {
+        additionalData: '@import "@/assets/style/global/variables.scss";',
+      },
+    },
+  },
+  configureWebpack: {
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, 'src')
+      },
+    },
+  },
+};
